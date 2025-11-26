114661021183485891ef23337f6a3b8a0d03f1b4
Eoin Ocathasaigh <eoinocathasaigh1@gmail.com>
2025-11-26 16:24:18 +0000

    Added more changes to fix some errors

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -4,8 +4,10 @@ This module exposes `app` for tools like the `flask` command and imports
 `views` for their side-effects (route registration).
 """
 
-from . import app  # For application discovery by the 'flask' command.  # noqa: F401
-from . import views  # For import side-effects of setting up routes.  # noqa: F401
+# Keep these imports for application discovery and route registration.
+# They are intentionally unused in this module body.
+from . import app  # noqa: F401
+from . import views  # noqa: F401
 
 # Time-saver: uncomment to quickly open the example URL in a local terminal
 # print('http://127.0.0.1:5000/hello/VSCode')
